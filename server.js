5ea456e8d362ce5ea0b99e291ab21f2ac00b0603
Keerthisiri2693 <keerthisiri2693@gmail.com>
2026-07-03 17:05:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,37 +6,27 @@ dotenv.config();
 
 const connectDB = require("./config/db");
 
-// Routes
-const userRoutes = require("./routes/userRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-
 const app = express();
 
 // Connect MongoDB
 connectDB();
 
 // Middleware
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
 // Routes
-app.use("/api/users", userRoutes);
-app.use("/api/admin", adminRoutes);
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/admin", require("./routes/adminRoutes"));
 
-// Test Route
+// Home
 app.get("/", (req, res) => {
-  res.send("Node Server Running...");
+  res.send("AST Billing Backend Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server Running on Port ${PORT}`);
 });
